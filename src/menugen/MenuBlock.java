b893445e1af2e7d627e959472004aed1cd9f6ef7
ecke101 <ecke101@gmail.com>
2011-06-03 13:48:03 +0000

    First test commit

---
+++ src/menugen/MenuBlock.java
@@ -0,0 +1,46 @@
+package menugen;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class MenuBlock {
+
+	static ArrayList<MenuBlock> blocks = new ArrayList<MenuBlock>();
+	
+	public int x, y;
+	public String header;
+	public ArrayList<MenuItem> items;
+	//public boolean selected = false; 
+	
+	public MenuBlock() {
+		items = new ArrayList<MenuItem>();
+		x = 60;
+		y = 60;
+		header = "default";
+	}
+	
+	public int getHeight() {
+		return (26 + items.size() * MainPanel.ITEM_H);
+	}
+	
+	public boolean isInside(Point p) {
+		if (p.getX() > x && p.getX() < (x + 100) &&
+				p.getY() > y && p.getY() < (y + 26 + items.size() * MainPanel.ITEM_H)) 
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public MenuItem hitItem(int item_x, int item_y) {
+		if (item_x > (x + 3) && item_y > (y + 26)) {
+			System.out.println(item_y - (y + 26));
+			return items.get((item_y - (y + 26)) / MainPanel.ITEM_H);
+			
+			
+		}
+		return null;
+	}
+}
---
+++ src/menugen/MenuItemText.java
@@ -0,0 +1,9 @@
+package menugen;
+
+public class MenuItemText extends MenuItem {
+
+	public MenuItemText(String caption) {
+		super(caption, MenuItem.TYPE_TEXT);
+	}
+
+}
